a1be0a6bc4570e31192cc9a0336a7b40174fc363
Wahyu Sampurno Adi <wahyusampurnoadi@gmail.com>
2023-07-15 17:08:39 +0700

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var buttonWrap = document.getElementById("button");
+const buttonWrap = document.getElementById("button");
 
-var btnNum = buttonWrap.getElementsByClassName("btn-num");
+const btnNum = buttonWrap.getElementsByClassName("btn-num");
 
 for (var i = 0; i < btnNum.length; i++) {
   btnNum[i].addEventListener("click", function () {
@@ -9,3 +9,29 @@ for (var i = 0; i < btnNum.length; i++) {
     this.className += " active";
   });
 }
+
+function rating() {
+  const body = document.getElementsByTagName(body);
+  body.classList.toggle("thank-you");
+}
+
+const submitBtn = document.getElementById("submit");
+const rateCard = document.getElementById("card");
+const thanksCard = document.querySelector(".thank-you");
+const result = document.getElementById("rating");
+const buttonNum = document.querySelectorAll(".btn-num");
+
+let click = false;
+buttonNum.forEach((number) => {
+  number.addEventListener("click" , () =>{
+    result.innerHTML = number.innerHTML;
+    click = true;
+  })
+})
+
+submitBtn.addEventListener("click", () => {
+  if(click){
+    thanksCard.classList.remove("hidden");
+    rateCard.classList.add("hidden");
+  }
+});
